02737f89d38a6f596ee0f872f9a458b8ae57b1b2
Aryan Karan <76065891+aryankaran@users.noreply.github.com>
2021-05-07 23:02:20 +0530

    Latest build vars updatd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,18 +16,17 @@
 # limitations under the License.
 #
 
-add_lunch_combo omni_Cloud_Q11-userdebug
 add_lunch_combo omni_Cloud_Q11-eng
+add_lunch_combo omni_Cloud_Q11-userdebug
 
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export TARGET_ARCH=arm
 export OF_TARGET_DEVICES="CloudQ11,Cloud_Q11,aryan,Cloud Q11"
 export TARGET_DEVICE_ALT="Cloud Q11"
-export OF_USE_MAGISKBOOT=1
 export OF_DEVICE_WITHOUT_PERSIST=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-export OF_PATCH_AVB20=1
+#export OF_PATCH_AVB20=1
 export OF_QUICK_BACKUP_LIST="/data;/boot;/system"
 export OF_USE_TWRP_SAR_DETECT=1
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
@@ -35,8 +34,7 @@ export OF_TWRP_COMPATIBILITY_MODE=1
 export OF_NO_MIUI_OTA_VENDOR_BACKUP=1
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 export OF_MAINTAINER="Aryan Karan"
-export FOX_VERSION="R11.1_3"
-export BUILD_2GB_VERSION=0
+export FOX_VERSION="R11.1_Aryan"
 export LC_ALL="C"
 #new
 export FOX_USE_NANO_EDITOR=1
@@ -48,3 +46,4 @@ export OF_DONT_KEEP_LOG_HISTORY=1
 export OF_USE_LOCKSCREEN_BUTTON=1
 export FOX_USE_GREP_BINARY=1
 export FOX_USE_XZ_UTILS=1
+export FOX_REPLACE_TOOLBOX_GETPROP=1
