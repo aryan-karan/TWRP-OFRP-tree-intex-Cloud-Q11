5739ffcba5e504e50f44224afe3c36a2da0576a8
Aryan Karan <76065891+aryankaran@users.noreply.github.com>
2021-05-07 23:30:17 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,15 +21,29 @@ add_lunch_combo omni_Cloud_Q11-userdebug
 
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export LC_ALL="C"
-export OF_MAINTAINER="Aryan Karan"
-export FOX_USE_NANO_EDITOR=1
 export TARGET_ARCH=arm
-export FOX_REPLACE_TOOLBOX_GETPROP=1
-export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/mtk-msdc.0/11120000.msdc0/by-name/recovery"
-export FOX_USE_TAR_BINARY=1
+export OF_TARGET_DEVICES="CloudQ11,Cloud_Q11,aryan,Cloud Q11"
+export TARGET_DEVICE_ALT="Cloud Q11"
+export OF_DEVICE_WITHOUT_PERSIST=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-export FOX_RESET_SETTINGS=0
-export FOX_VERSION="R10_Aryan"
+#export OF_PATCH_AVB20=1
+export OF_QUICK_BACKUP_LIST="/data;/boot;/system"
+export OF_USE_TWRP_SAR_DETECT=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
+export OF_TWRP_COMPATIBILITY_MODE=1
+export OF_NO_MIUI_OTA_VENDOR_BACKUP=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+export OF_MAINTAINER="Aryan Karan"
+export FOX_VERSION="R11.1"
+export LC_ALL="C"
+#new
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_TAR_BINARY=1
+#torch OF_FL_PATH1 & OF_FL_PATH2
+#export OF_FL_PATH1="/sys/class/flashlightdrv/dev"
 export OF_DONT_KEEP_LOG_HISTORY=1
+export OF_USE_LOCKSCREEN_BUTTON=1
+export FOX_USE_GREP_BINARY=1
 export FOX_USE_XZ_UTILS=1
+export FOX_REPLACE_TOOLBOX_GETPROP=1
