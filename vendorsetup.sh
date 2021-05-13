de207c73c46ea9c9ca246b952067e15e6119c91f
Aryan Karan <76065891+aryankaran@users.noreply.github.com>
2021-05-13 14:19:31 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -72,3 +72,6 @@ export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/platform/mtk-msdc.0/11120000.ms
 
 export FOX_DISABLE_APP_MANAGER=1
 export OF_FLASHLIGHT_ENABLE=0
+
+# Magisk 23.0
+export FOX_USE_SPECIFIC_MAGISK_ZIP=$(DEVICE_PATH)/magisk/
